0248dcabd559f900290e98c97512a51552030542
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2021-03-15 08:11:54 +0300

    Initial commit

---
+++ day-1-2-test-your-code-main.py
@@ -0,0 +1,8 @@
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
---
+++ day-1-3-test-your-code#main.py
@@ -0,0 +1,4 @@
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
+
+print(len(input("what is your name?)))
